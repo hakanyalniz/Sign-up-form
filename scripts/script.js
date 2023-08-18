46dbfcb2733517340a6c7edcad1686c66fe6b1f4
hakanyalniz <hakan_yalniz@live.com>
2023-08-18 20:22:51 +0300

    Finished the fade out slideshow, added some comments and cleaned up code

--- scripts/script.js
+++ scripts/script.js
@@ -1,73 +1,39 @@
 "use strict";
 
-/*
-  since querySelectorAll gives us the total number
-  of items of the class "controls"
-  console.log(controls.length) result(3)
-*/
-let slides = document.querySelectorAll("#all_slides .slide");
-let currentSlide = 0;
-
-const nextSlide = () => {
-  goToSlide(currentSlide + 1);
-};
-
-const previousSlide = () => {
-  goToSlide(currentSlide - 1);
-};
-
-/*
-  this does the magic of moving to the next slide after
-  every 2.1 seconds and starting all over.
-*/
-
-let slideInterval = setInterval(nextSlide, 10000);
-
 // which slide to move to as denoted by "s"
-const goToSlide = (s) => {
-  /* 
-   when this function is run, hide all slides
-   since the slide class has opacity set to 0.
-  */
+// all the slides are hidden by default, since they have opacity 0
+function goToSlide(s) {
+
+  // this will reset the class name back to just slide, deleting active, if it was there
   slides[currentSlide].className = "slide";
 
-  /* assuming we have 4 different slides
-        we can index these slides as follows:
-        Slide 1 = 0
-        Slide 2 = 1
-        Slide 3 = 2
-        Slide 4 = 3
 
-        Now, we set the current slide index to be zero.
-
-        currentSlide = 0
-
-        Now, we need to find a way to increment the currentSlide index 
-        every 2.1 seconds as specified in our setInterval method.
-
-        recall the slide.length = 4
-        This tells us that the index stops at 3.
-
-        currentSlide = 0 + 4/4 = 1 [new currentSlide]
-
-        now that the currentSlide is 1, when the setInterval runs 
-        it again, we have:
-
-        currentSlide = 1 + 4/4 = 2 [new currentSlide]
-
-        now that the currentSlide is 2, when the setInterval runs 
-        it again, we have:
-
-        currentSlide = 2 + 4/4 = 3 [new currentSlide]
-
-        since our index stops at 3. The setInternal starts from the
-        beginning again by assigning 0 to currentSlide and doing same 
-        process all over again.
-
-     */
+  // the following currentSlide is sort of an advanced version of currentIndex++
+  // it works by modulo operator, which results in the remainder of two divided numbers
+  // so: 4 % 2 is 0, 5 % 2 is 1, 1 % 5 is 1
+  // because, 4 divided by 2 leaves no remainder, 5 divided by 2 leaves 1 remainder and 1 can't be divided by 5
+  // so the 1 remains as is, if it was 2 % 5 then the remainder would be 2 and so on
+  // to understand the statement below better let me explain a few cycles
+  // at the beginning s = 1, since currentSlide = 0, and slides.length is 6
+  // (1 + 6) % 6 => 7 % 6 = 1 <<< currentSlide is now equal to 1, when it is called next time it will be +1 from goToSlide
+  // (2 + 6) % 6 => 8 % 6 = 2
+  // (3 + 6) % 6 => 9 % 6 = 3
+  // and so on, the s increases because each interval the currentSlide is made equal to the previous result
+  // at the end when s becomes 6 it will be ... (6 + 6) % 6 => 12 % 6 = 0
+  // so it will reset automatically
   currentSlide = (s + slides.length) % slides.length;
   // in active we set opacity to 1.
   slides[currentSlide].className = "slide active";
 };
 
-let playing = true;
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+
+}
+
+let slides = document.querySelectorAll("#all_slides .slide");
+let currentSlide = 0;
+
+// This triggers everything, the interval below can be changed to change slide timings
+setInterval(nextSlide, 10000);
+
